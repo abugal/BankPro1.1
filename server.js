b853dc156e9672d739c037c075c369537daa6581
Abubakar Aliyu <89919804+abugal@users.noreply.github.com>
2023-05-17 17:02:26 +0200

    new connect to mongoose

--- server.js
+++ server.js
@@ -1,50 +1,52 @@
-require('dotenv').config()
-
-const mongoose = require('mongoose')
-const express = require('express')
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
 const path = require("path");
-const genLedgerRoutes = require('./routes/genLedger')
-const depositRoutes = require('./routes/deposit')
-const userRoutes = require('./routes/user')
-
+const genLedgerRoutes = require("./routes/genLedger");
+const depositRoutes = require("./routes/deposit");
+const userRoutes = require("./routes/user");
 
 // initialise the express App
 const app = express();
+const PORT = process.env.PORT || 500;
 
 // register a global middleware
-app.use(express.json()) // to be use in accessing the request body
+app.use(express.json()); // to be use in accessing the request body
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
+  console.log(req.path, req.method);
+  next();
+});
 
-})
 
-// setup a route handler
+
+// serving the front end
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
+
+
+  // setup a route handler
 //app.use('/api/genLedger', genLedgerRoutes)
 app.use("/api/deposit", depositRoutes);
 app.use("/api/user", userRoutes);
 
-// serving the front end
-app.use(express.static(path.join(__dirname, './client/build')));
-app.get("*", function(_, res){
-    res.sendFile(
-        path.join(__dirname, "./client/build/index.html"),
-        function (err) {
-            res.status(500).send(err);
-        }
-    );
-});
-
-
 /// connect to the database
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     // Listen for a request in a certain port number.
     // through the use of the .env
-    app.listen(process.env.PORT, () => {
-        console.log('connect to MongoDB and listening on port', process.env.PORT)
-    })
-})
-.catch((error) => {
-    console.log(error)
-})
+    app.listen(PORT, () => {
+      console.log("connect to MongoDB and listening on port");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
